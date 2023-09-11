9ac09c24b63e27a849708167c31b1e696961e15d
Miguel Flores <mflores9203@bths.edu>
2023-09-11 01:16:42 -0400

    Complete plane animation

--- components/NewsletterForm.tsx
+++ components/NewsletterForm.tsx
@@ -1,33 +1,66 @@
 "use client";
 
 import { EnvelopeIcon } from "@heroicons/react/24/solid";
-import { useRef, useState } from "react";
+import { FormEvent, useRef, useState } from "react";
+import { gsap } from "gsap";
+import { getPlaneKeyframes } from "@/lib/getPlaneKeyframes";
 
 function NewsletterForm() {
   const [input, setInput] = useState("");
-  const buttonRef = useRef<HTMLButtonElement>(null)
+  const [active, setActive] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const { to, fromTo, set } = gsap;
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const email = input;
+    const button = buttonRef.current;
+
+    if (!email || !button) return;
+
+    if (!active) {
+      setActive(true);
+
+      // to gsap animation - for plane
+      to(button, {
+        keyframes: getPlaneKeyframes(set, fromTo, button, setActive, setInput),
+      });
+
+      // to gsap animation - for trails
+      to(button, {
+        keyframes: getTrailsKeyframes(set, fromTo, button, setActive, setInput),
+      });
+    }
+  };
 
   return (
     <div className="flex flex-col space-y-8 md:w-[400px]">
-      <form action="" className="newsletter-form mt-10 animate-fade-in-3">
-        <div className="group flex items-center gap-x-4 py-1 pl-4 pr-1 rounded-[9px] bg-[#090D11] hover:bg-[#15141B] shadow-outline-gray 
-        hover:shadow-transparent focus-within:bg-[#15141B]
-        focus-within:!shadow-outline-gray-focus transition-all duration-300">
-          <EnvelopeIcon className="hidden sm:inline w-6 h-6 text-[#4B4V52] group-focus-within:text-white
-          group-hover:text-white transition-colors duration-300"/>
+      <form
+        onSubmit={handleSubmit}
+        className="newsletter-form mt-10 animate-fade-in-3"
+      >
+        <div className="group flex items-center gap-x-4 py-1 pl-4 pr-1 rounded-[9px] bg-[#090D11] hover:bg-[#15141B] shadow-outline-gray hover:shadow-transparent focus-within:bg-[#15141B] focus-within:!shadow-outline-gray-focus transition-all duration-300">
+          <EnvelopeIcon className="hidden sm:inline w-6 h-6 text-[#4B4V52] group-focus-within:text-white group-hover:text-white transition-colors duration-300" />
           <input
+            className="flex-1 text-white text-sm sm:text-base outline-none placeholder-[#4B4C52] group-focus-within:placeholder-white bg-transparent placeholder:transition-colors
+placeholder:duration-300"
             value={input}
             onChange={(e) => setInput(e.target.value)}
             type="email"
             placeholder="Email address"
             required
-            className="flex-1 text-white text-sm sm:text-base outline-none placeholder:[#4B4C52] 
-            group-focus-within:placeholder-white bg-transparent placeholder:transition-colors\
-            placeholder:duration-300"
-
           />
-          <button ref={buttonRef} disabled={!input} type="submit">
-          <span className="default">Subscribe</span>
+          <button
+            ref={buttonRef}
+            type="submit"
+            className={`${
+              active && "active"
+            } disabled:!bg-[#17141F] disabled:grayscale-[65%]
+            disabled:opacity-50 disabled:cursor-not-allowed text-sm md:text-base`}
+            disabled={!input}
+          >
+            <span className="default">Subscribe</span>
             <span className="success">
               <svg viewBox="0 0 16 16">
                 <polyline points="3.75 9 7 12 13 5"></polyline>
